5592af047cf19ee3b1d8c221d0bda3e5ffca6239
sneyder20191005 <>
2021-04-22 19:49:55 +0000

    reto14

---
+++ py2_retos/reto14.py
@@ -0,0 +1,7 @@
+leer = int(input("digite el numero al cual quiere saber el cuadrado"))
+def cuadrado(numero):
+  print(pow(numero,2))
+def mensaje (numero):
+  print("programa finalizado")
+cuadrado(leer)
+mensaje(leer)
