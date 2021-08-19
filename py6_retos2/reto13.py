179659100627e092eeefc467ac21998389c33890
sneyder20191005 <81379348+sneyder20191005072@users.noreply.github.com>
2021-08-19 20:07:38 +0000

    reto 13 (:

---
+++ py6_retos2/reto13.py
@@ -0,0 +1,6 @@
+numero1= float(input("¿Cuál es el primer número?")) 
+numero2 = float(input("¿Cuál es el segundo número?"))
+media= (numero1+numero2)/2
+print('Primer número:',numero1)
+print('Segundo número:',numero2)
+print('Su media:',media)
