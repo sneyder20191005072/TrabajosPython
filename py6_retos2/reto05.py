2d0853c6eaafd1c4d9687e96f41ffb7b11ba315a
sneyder20191005 <81379348+sneyder20191005072@users.noreply.github.com>
2021-08-19 19:38:31 +0000

    reto 5

--- py6_retos2/reto05.py
+++ py6_retos2/reto05.py
@@ -1,23 +1,3 @@
-b=1
-#i=1
-lista =[]
-numero = int(input('ingrese el numero a evaluar :'))
-c = numero % 2
-if c == 0:
- lista.append(numero)
- b=2
- 
-if b ==1:
- lista.append(numero)
- numero= numero *3 +1
- lista.append(numero)
-while numero != 1: #i >0:
- numero=numero // 2
- lista.append(numero)
- #if numero == 1:
- # i=0
- #else:
- # i=1
-#[6,3,10,5,16,4,2,1]
-print("la lista de numero es :")
-print(lista)
+import random
+x=random.randrange(101)
+print('Número elegido:',x)
